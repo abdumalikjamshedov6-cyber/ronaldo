145eebcfc544222f64c015e265d3f5376db8ec57
abdumalikjamshedov6-cyber <abdumalikjamshedov6@gmail.com>
2026-07-17 10:02:41 +0500

    first commit

---
+++ rasm.js
@@ -0,0 +1,13 @@
+let box = document.querySelectorAll(".box");
+
+for(let i = 0; i < 1; i++){
+    box[i].innerHTML = '<img src="ronaldo/1.jpg">';
+    box[i+1].innerHTML = '<img src="ronaldo/2.jpg">';
+    box[i+2].innerHTML = '<img src="ronaldo/3.jpg">';
+    box[i+3].innerHTML = '<img src="messi/1.jpg">';
+    box[i+4].innerHTML = '<img src="messi/2.jpg">';
+    box[i+5].innerHTML = '<img src="messi/3.jpg">';
+    box[i+6].innerHTML = '<img src="neymar/1.jpg">';
+    box[i+7].innerHTML = '<img src="neymar/2.jpg">';
+    box[i+8].innerHTML = '<img src="neymar/3.jpg">';
+}
